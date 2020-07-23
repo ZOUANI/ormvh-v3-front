96e0ea15123cb9ee40fa8cf27d67e6c396677019
Ali <arabat50@gmail.com>
2020-07-23 15:37:41 +0100

    Improve the left side bar style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {MenuItem} from "primeng/api";
+import { MenuItem } from "primeng/api";
 
 @Component({
   selector: 'app-root',
@@ -8,74 +8,95 @@ import {MenuItem} from "primeng/api";
 })
 export class AppComponent {
   title = 'frontend';
-   sidebarItems: MenuItem[];
-   openBar: Boolean;
+  sidebarItems: MenuItem[];
+  openBar: Boolean;
 
-   notifications : string[];  //contain the notifications to show
-   badgeNumber : number;      //contain the number of notifications , if =0 the badge won't show
+  notifications: string[];  //contain the notifications to show
+  badgeNumber: number;      //contain the number of notifications , if =0 the badge won't show
   settingItems: MenuItem[];  //contain items for the setting menu on nav bar
 
   constructor() {
-    this.openBar=false;
+    this.openBar = false;
 
-     // to change icons , change the icon value on the array below to one of the font awesome classes 
-    this.sidebarItems= [{
-      items :[
-                { label: 'Status', routerLink: '/status/list' ,icon:'fa fa-database' },
-        { label: 'CourrierObject', routerLink: '/courrierObject/list' ,icon:'fa fa-database' },
-        { label: 'Evaluation', routerLink: '/evaluation/list' ,icon:'fa fa-database' },
-        { label: 'Task', routerLink: '/task/list' ,icon:'fa fa-database' },
-        { label: 'User', routerLink: '/user/list' ,icon:'fa fa-database' },
-        { label: 'NatureCourrier', routerLink: '/natureCourrier/list' ,icon:'fa fa-database' },
-        { label: 'ExpeditorType', routerLink: '/expeditorType/list' ,icon:'fa fa-database' },
-        { label: 'CourrierServiceItem', routerLink: '/courrierServiceItem/list' ,icon:'fa fa-database' },
-        { label: 'Courrier', routerLink: '/courrier/list' ,icon:'fa fa-database' },
-        { label: 'ModelLettreReponse', routerLink: '/modelLettreReponse/list' ,icon:'fa fa-database' },
-        { label: 'Subdivision', routerLink: '/subdivision/list' ,icon:'fa fa-database' },
-        { label: 'Nationality', routerLink: '/nationality/list' ,icon:'fa fa-database' },
-        { label: 'TypeCourrier', routerLink: '/typeCourrier/list' ,icon:'fa fa-database' },
-        { label: 'Expeditor', routerLink: '/expeditor/list' ,icon:'fa fa-database' },
-        { label: 'Role', routerLink: '/role/list' ,icon:'fa fa-database' },
-        { label: 'Employee', routerLink: '/employee/list' ,icon:'fa fa-database' },
-        { label: 'CategorieModelLettreReponse', routerLink: '/categorieModelLettreReponse/list' ,icon:'fa fa-database' },
-        { label: 'LeService', routerLink: '/leService/list' ,icon:'fa fa-database' },
-        { label: 'Sexe', routerLink: '/sexe/list' ,icon:'fa fa-database' },
-        { label: 'Bordereau', routerLink: '/bordereau/list' ,icon:'fa fa-database' },
-        { label: 'Voie', routerLink: '/voie/list', icon:'fa fa-database' }
+    // to change icons , change the icon value on the array below to one of the font awesome classes 
+    this.sidebarItems = [{
+      items: [
+        {
+          id: 'courrier', label: 'Courrier', icon: 'fa fa-suitcase', items: [
+            { label: 'Courrier', routerLink: '/courrier/list', icon: 'fa fa-suitcase' },
+            { label: 'Object', routerLink: '/courrierObject/list', icon: 'fa fa-header' },
+            { label: 'Nature', routerLink: '/natureCourrier/list', icon: 'fa fa-list' },
+            { label: 'Service Item', routerLink: '/courrierServiceItem/list', icon: 'fa fa-wrench' },
+            { label: 'Type', routerLink: '/typeCourrier/list', icon: 'fa fa-sticky-note-o' },
+
+          ]
+        },
+        {
+          id: 'utilisateurs', label: 'Utilisateurs', icon: 'fa fa-users', items: [
+            { label: 'Utilisateurs', routerLink: '/user/list', icon: 'fa fa-user' },
+            { label: 'Rôles', routerLink: '/role/list', icon: 'fa fa-tag' },
+          ]
+        },
+        { id: 'employé', label: 'Employé', routerLink: '/employee/list', icon: 'fa fa-male' },
+        {
+          id: 'services', label: 'Services', icon: 'fa fa-briefcase', items: [
+            { label: 'LeService', routerLink: '/leService/list', icon: 'fa fa-briefcase' },
+            { label: 'Evaluation', routerLink: '/evaluation/list', icon: 'fa fa-check-square-o' },
+            { label: 'Tâche', routerLink: '/task/list', icon: 'fa fa-tasks' },
+            { label: 'Bordereau', routerLink: '/bordereau/list', icon: 'fa fa-file-text-o' },
+          ]
+        },
+        {
+          id: 'lettres', label: 'Lettres', icon: 'fa fa-envelope', items: [
+            { label: 'Voie', routerLink: '/voie/list', icon: 'fa fa-paper-plane' },
+            { label: 'Expéditeur', routerLink: '/expeditor/list', icon: 'fa fa-ship' },
+            { label: 'Expéditeur Type', routerLink: '/expeditorType/list', icon: 'fa fa-archive' },
+            { label: 'Catégorie Modele Lettre Réponse', routerLink: '/categorieModelLettreReponse/list', icon: 'fa fa-filter' },
+            { label: 'Modèle Lettre Reponse', routerLink: '/modelLettreReponse/list', icon: 'fa fa-envelope-square' },
+          ]
+        },
+        { id: 'status', label: 'Status', routerLink: '/status/list', icon: 'fa fa-exclamation-circle' },
+        {
+          id: 'paramètres', label: 'Paramètres', icon: 'fa fa-gear', items: [
+            { label: 'Subdivision', routerLink: '/subdivision/list', icon: 'fa fa-exchange' },
+            { label: 'Nationalité', routerLink: '/nationality/list', icon: 'fa fa-globe' },
+            { label: 'Sexe', routerLink: '/sexe/list', icon: 'fa fa-genderless' },
+          ]
+        },
       ]
     }];
 
-this.settingItems=[
+    this.settingItems = [
       {
         label: '',
-        items:[
-          { label: 'settings', routerLink:'/', icon:'fa fa-cog'},
-          { label: 'profile', routerLink:'/', icon:'fa fa-user'}]
+        items: [
+          { label: 'settings', routerLink: '/', icon: 'fa fa-cog' },
+          { label: 'profile', routerLink: '/', icon: 'fa fa-user' }]
       },
       {
         label: '',
-        items:[
-          { label: 'Log out', routerLink:'/', icon:'fa fa-sign-out'}]
+        items: [
+          { label: 'Log out', routerLink: '/', icon: 'fa fa-sign-out' }]
       }
     ];
-    
-    this.notifications= ["Test 1", "Use equal casing. Compare these module identifiers:","There are multiple modules with names that only differ in casing.\n" +
-    "This can lead to unexpected behavior when compiling on a filesystem with other case-semantic." ];
+
+    this.notifications = ["Test 1", "Use equal casing. Compare these module identifiers:", "There are multiple modules with names that only differ in casing.\n" +
+      "This can lead to unexpected behavior when compiling on a filesystem with other case-semantic."];
 
     this.badgeNumber = this.notifications.length;
 
 
-}
+  }
 
   buttonClick() {
-    if(this.openBar == true) {
+    if (this.openBar == true) {
       this.openBar = false;
-      console.log("if "+this.openBar);
-    }    else
-      {  this.openBar=true;
-        console.log("else "+this.openBar);
-      }
-    console.log("none "+this.openBar);
+      console.log("if " + this.openBar);
+    } else {
+      this.openBar = true;
+      console.log("else " + this.openBar);
+    }
+    console.log("none " + this.openBar);
   }
 
   markRead() {
