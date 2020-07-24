0a177d8f176528f32ad73ae596244e87b2e347ca
Ali <arabat50@gmail.com>
2020-07-24 17:15:53 +0100

    Added internationalisation i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MenuItem } from "primeng/api";
 import { AuthenticationService } from './controller/service/auth/authentication.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -17,63 +18,74 @@ export class AppComponent {
   sidebarItems: MenuItem[];
   openBar: Boolean;
 
+
   notifications: string[];  //contain the notifications to show
   badgeNumber: number;      //contain the number of notifications , if =0 the badge won't show
+  languageItems: MenuItem[];  //contain items for the language menu on nav bar
   settingItems: MenuItem[];  //contain items for the setting menu on nav bar
 
-  constructor(private authService: AuthenticationService) {
+  languages: { abbrv: string, label: string }[] = [
+    { abbrv: 'en', label: 'English' },
+    { abbrv: 'fr', label: 'French' },
+    { abbrv: 'ar', label: 'Arabic' }
+  ];
+
+  constructor(private authService: AuthenticationService, public translateService: TranslateService) {
+    translateService.addLangs(this.languages.map(lg => lg.abbrv).concat());
+    translateService.setDefaultLang('en');
+    const browserLang = translateService.getBrowserLang();
+    translateService.use(browserLang.match(/en|fr|ar/) ? browserLang : 'en');
     this.openBar = false;
 
     // to change icons , change the icon value on the array below to one of the font awesome classes
     this.sidebarItems = [{
       items: [
         {
-          id: 'courrier', label: 'Courrier', icon: 'fa fa-suitcase', items: [
-            { label: 'Courrier', routerLink: '/courrier/list', icon: 'fa fa-suitcase' },
-            { label: 'Object', routerLink: '/courrierObject/list', icon: 'fa fa-header' },
-            { label: 'Bordereau', routerLink: '/bordereau/list', icon: 'fa fa-file-text-o' },
-            { label: 'Expéditeur', routerLink: '/expeditor/list', icon: 'fa fa-ship' },
-            { label: 'Evaluation', routerLink: '/evaluation/list', icon: 'fa fa-check-square-o' },
+          id: 'mailing', label: 'MAILING', icon: 'fa fa-suitcase', items: [
+            { label: 'MAILING', routerLink: '/courrier/list', icon: 'fa fa-suitcase' },
+            { label: 'OBJECT', routerLink: '/courrierObject/list', icon: 'fa fa-header' },
+            { label: 'STATEMENT', routerLink: '/bordereau/list', icon: 'fa fa-file-text-o' },
+            { label: 'EXPEDITOR', routerLink: '/expeditor/list', icon: 'fa fa-ship' },
+            { label: 'EVALUATION', routerLink: '/evaluation/list', icon: 'fa fa-check-square-o' },
             // { label: 'Service Item', routerLink: '/courrierServiceItem/list', icon: 'fa fa-wrench' },
+            // { label: 'Tâche', routerLink: '/task/list', icon: 'fa fa-tasks' },
           ]
         },
+
+        { id: 'users', label: 'USERS', routerLink: '/user/list', icon: 'fa fa-user' },
+
         {
-          id: 'utilisateurs', label: 'Utilisateurs', icon: 'fa fa-users', items: [
-            { label: 'Utilisateurs', routerLink: '/user/list', icon: 'fa fa-user' },
-            { label: 'Rôles', routerLink: '/role/list', icon: 'fa fa-tag' },
+          id: 'letters', label: 'LETTERS', icon: 'fa fa-envelope', items: [
+            { label: 'CATEGORY', routerLink: '/categorieModelLettreReponse/list', icon: 'fa fa-filter' },
+            { label: 'MODEL', routerLink: '/modelLettreReponse/list', icon: 'fa fa-envelope-square' },
           ]
         },
-        // {
-        //   id: 'services', label: 'Services', icon: 'fa fa-briefcase', items: [
-
-
-        // { label: 'Tâche', routerLink: '/task/list', icon: 'fa fa-tasks' },
-
-        //   ]
-        // },
+        { id: 'employee', label: 'EMPLOYEE', routerLink: '/employee/list', icon: 'fa fa-male' },
         {
-          id: 'lettres', label: 'Lettres', icon: 'fa fa-envelope', items: [
-
-            { label: 'Catégorie', routerLink: '/categorieModelLettreReponse/list', icon: 'fa fa-filter' },
-            { label: 'Modèle', routerLink: '/modelLettreReponse/list', icon: 'fa fa-envelope-square' },
-          ]
-        },
-        { id: 'employé', label: 'Employé', routerLink: '/employee/list', icon: 'fa fa-male' },
-        {
-          id: 'paramètres', label: 'Paramètres', icon: 'fa fa-gear', items: [
-            { label: 'Service', routerLink: '/leService/list', icon: 'fa fa-briefcase' },
-            { label: 'Subdivision', routerLink: '/subdivision/list', icon: 'fa fa-exchange' },
-            { label: 'Nature', routerLink: '/natureCourrier/list', icon: 'fa fa-list' },
-            { label: 'Type', routerLink: '/typeCourrier/list', icon: 'fa fa-sticky-note-o' },
-            { label: 'Voie', routerLink: '/voie/list', icon: 'fa fa-paper-plane' },
-            { label: 'Status', routerLink: '/status/list', icon: 'fa fa-exclamation-circle' },
-            { label: 'Expéditeur Type', routerLink: '/expeditorType/list', icon: 'fa fa-archive' },
-            { label: 'Nationalité', routerLink: '/nationality/list', icon: 'fa fa-globe' },
-            { label: 'Sexe', routerLink: '/sexe/list', icon: 'fa fa-genderless' },
+          id: 'configuration', label: 'CONFIGURATION', icon: 'fa fa-gear', items: [
+            { label: 'SERVICE', routerLink: '/leService/list', icon: 'fa fa-briefcase' },
+            { label: 'SUBDIVISION', routerLink: '/subdivision/list', icon: 'fa fa-exchange' },
+            { label: 'NATURE', routerLink: '/natureCourrier/list', icon: 'fa fa-list' },
+            { label: 'TYPE', routerLink: '/typeCourrier/list', icon: 'fa fa-sticky-note-o' },
+            { label: 'TRACK', routerLink: '/voie/list', icon: 'fa fa-paper-plane' },
+            { label: 'STATUS', routerLink: '/status/list', icon: 'fa fa-exclamation-circle' },
+            { label: 'EXPEDITOR_TYPE', routerLink: '/expeditorType/list', icon: 'fa fa-archive' },
+            { label: 'ROLES', routerLink: '/role/list', icon: 'fa fa-tag' },
+            { label: 'NATIONALITY', routerLink: '/nationality/list', icon: 'fa fa-globe' },
+            { label: 'GENDER', routerLink: '/sexe/list', icon: 'fa fa-genderless' },
           ]
         },
       ]
     }];
+
+    this.languageItems = [
+      {
+        label: '',
+        items: this.languages.map(lg => ({ label: lg.label, routerLink: [], command: () => translateService.use(lg.abbrv) })).concat()
+        ,
+
+      }
+    ];
 
     this.settingItems = [
       {
