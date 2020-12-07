e8d7a22b3013acf181a2416d689c37c96f978044
MoulaYounes <zouani.younes@gmail.com>
2020-12-08 00:24:26 +0100

    fix file-saver prob

--- src/app/controller/service/Expeditor.service.ts
+++ src/app/controller/service/Expeditor.service.ts
@@ -9,6 +9,7 @@ import {SelectItem} from "primeng";
 import {NatureCourrierEditComponent} from "../../view/natureCourrier/edit/natureCourrier-edit.component";
 import {NatureClientVo} from "../model/NatureClient.model";
 import {PhaseAdminVo} from "../model/PhaseAdmin.model";
+import {ExpeditorTypeVo} from "../model/ExpeditorType.model";
 
 @Injectable({
     providedIn: 'root'
@@ -27,6 +28,8 @@ export class ExpeditorService {
     private _searchedExpeditors: Array<ExpeditorVo> = new Array<ExpeditorVo>();
     private _editableExpeditors: Array<ExpeditorVo> = new Array<ExpeditorVo>();
     private _sexes: Array<SexeVo> = new Array<SexeVo>();
+    private _expeditorTypeVos: Array<ExpeditorTypeVo> = new Array<ExpeditorTypeVo>();
+
     private _nationalitys: Array<NationalityVo> = new Array<NationalityVo>();
     private _createdBys: Array<UserVo> = new Array<UserVo>();
     private _updatedBys: Array<UserVo> = new Array<UserVo>();
@@ -44,6 +47,14 @@ export class ExpeditorService {
         this._sexes = value;
     }
 
+
+    get expeditorTypeVos(): Array<ExpeditorTypeVo> {
+        return this._expeditorTypeVos;
+    }
+
+    set expeditorTypeVos(value: Array<ExpeditorTypeVo>) {
+        this._expeditorTypeVos = value;
+    }
 
     get natureClients(): SelectItem[] {
         return this._natureClients;
@@ -241,6 +252,18 @@ export class ExpeditorService {
             }
         );
     }
+
+    public findAllExpeditorType() {
+        this.http.get<Array<ExpeditorTypeVo>>('http://localhost:8080/generated/expeditorType/').subscribe(
+            value => {
+                if (value != null) {
+                    this.expeditorTypeVos = value;
+                }
+            }
+        );
+    }
+
+
 
     public findAllcreatedBys() {
         this.http.get<Array<UserVo>>('http://localhost:8080/generated/user/').subscribe(
